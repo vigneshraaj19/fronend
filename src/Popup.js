3a07b777688fe2a29260cfa1d15b7885d88df80f
vigneshraaj <vigneshraaj19@gmail.com>
2022-10-31 22:29:57 +0530

    commit

--- src/Popup.js
+++ src/Popup.js
@@ -1,32 +1,52 @@
 import React from "react";
 import "./popup.css";
 import Segment from "./Segment";
+import { useState } from "react";
+
+
 
 function Popup(props) {
-  // var close=()=>{
-  //     props.setTrigger(false)
-  // }
+   
+    const [form, setform] = useState({});
 
-  return  (
+    const onUpdateField = (e) => {
+      e.preventDefault();
+      const nextFormState = {
+        ...form,
+        Segment_name: e.target.value,
+      
+
+      };
+      setform(nextFormState);
+    
+    }
+   const handleSubmit=()=>{
+    console.log(form)
+   }
+
+  return (
     <div className="popup">
       <div className="popup-inner">
-        <div className="topbar">Saving Segment</div>
-        <div className="textbox">
-          <div>Enter the Name of the Segment</div>
-          <input placeholder="Name of the Segment" />
-          <div>
-            To save your segment ,you need to addthe schemas to build the query
+        <h4 className="topbar">Saving Segment</h4>      
+          <div className="textbox">
+            <div className="textbox">Enter the Name of the Segment</div>
+            <input className="textbox" placeholder="Name of the Segment" required value={form.name}
+            onChange={onUpdateField} />
+            <div className="textboxs">
+              To save your segment ,you need to addthe schemas to build the
+              query
+            </div>
+            <Segment />
           </div>
-          <Segment />
+          <div className="footer">
+            <button type="submit" onClick={handleSubmit} className="close-btm">save the segment</button>
+            <button className="cancel-btm" onClick={props.setTrigger}>
+              cancel
+            </button>
+          </div>
         </div>
 
-        <div className="footer">
-          <button>save the segment</button>
-          <button className="close-btm" onClick={props.setTrigger}>
-            cancel
-          </button>
-        </div>
-      </div>
+     
     </div>
   );
 }
--- src/Segment.js
+++ src/Segment.js
@@ -1,67 +1,108 @@
 import React from "react";
 import "./segment.css";
 import { options } from "./Data.js";
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import Popup from "./Popup";
+
 
 function Segment() {
+ 
+  const [dataoptions, setdataoptions] = useState(options);
+  const [selectedoption, setselectedoption] = useState([]);
+  const [Unselectedoption, setUnselectedoption] = useState([]);
+  const [value, setValue] = useState("");
+  const [data, setdata] = useState([]);
 
-    const [dataoptions,setdataoptions]=useState(options);
-    const[ showdropdown,setshowdropdown]=useState(false);
-    const [selectedoption, setselectedoption] = useState([]);
-    const [Unselectedoption, setUnselectedoption] = useState([]);
-    const [data,setdata] = useState([]);
+  const clearState = () => {
+    setdata([]);
+  };
 
+  console.log(dataoptions);
 
-    const clearState = () => {
-       
-        setdata([])
+  var onselect = (e) => {
+    setValue(e.target.value);
+    console.log("dataoptions--->", dataoptions);
+    console.log(
+      "addselection--->",
+      e.target.value,
+      e.target.selectedIndex - 1,
+      dataoptions[e.target.selectedIndex]
+    );
+    e.preventDefault();
+    setdata((pre) => [...pre, dataoptions[e.target.selectedIndex - 1]]);
+  };
+
+  useEffect(() => {
+    if (Unselectedoption.length === 0) {
+      console.log("Unselected option");
+      setUnselectedoption(dataoptions);
     }
-    var onselect=(e)=>{
-        e.preventDefault();    
-        setdata(dataoptions[e.target.selectedIndex])  
-    }
+  }, []);
 
-    // console.log("selectedoption",selectedoption)
-
-   const addselection=()=>{
-    if(data!=[])
-    {
-    setselectedoption((selectedoption) => [
-        ...selectedoption,
-        data,
-      ]);
-    }else{
-      clearState()
+  const addselection = (e) => {
+    if (data.length > 0) {
+      setselectedoption((selectedoption) => [...selectedoption, data[0]]);
+      var filteredArray = Unselectedoption.filter((e) => e.id !== data[0].id);
+      console.log("filtered arrat------->", filteredArray);
+      setUnselectedoption(filteredArray);
+      setdataoptions(filteredArray);
+      setdata([]);
+    } else {
+      clearState();
     }
-      var result=dataoptions.filter(({ Value: id1 }) => !selectedoption.some(({ Value: id2 }) => id2 === id1));
-      setUnselectedoption(result);
-     
-   }
-   console.log("unselectedoption",Unselectedoption)
-   console.log("selectedoption",selectedoption)
+  };
+
+  var selectvalue = (e) => {
+    e.preventDefault();
   
+  };
+
+  console.log("unselectedoption", Unselectedoption);
+  console.log("selectedoption", selectedoption);
+
   return (
-   
-    <div  className="dropdown">
-    
-     {
-        showdropdown && selectedoption &&  <div className="blue-box">
-       
+    <div className="dropdown">
+         
+      {selectedoption.length > 0 && (
+        <div className="blue-box">
+          {selectedoption.map((options, index) => {
+            return (
+              <select onChange={selectvalue} value={options.Value}>
+                <option value={options.Value} >
+                  {options.Label}
+                </option>
+                {Unselectedoption.map((options, index) => (
+                  <option
+                    key={index}
+                    value={options.Value}
+                    disabled={options.isSelected}
+                  >
+                    {options.Label}
+                  </option>
+                ))}
+              </select>
+            );
+          })}
         </div>
-     }
-    
-      <select  onChange={onselect}  >
-      <option value="">Add schema to segment</option>
-
-        {Unselectedoption.map((options) => (
-          <option value={options.Value} disabled={options.isSelected}>{options.Label}</option>
+      )}
+      <select className="select" onChange={onselect} value={value}>
+        <option value="">Add schema to segment</option>
+        {Unselectedoption.map((options, index) => (
+          <option
+            key={index}
+            value={options.Value}
+            disabled={options.isSelected}
+          >
+            {options.Label}
+          </option>
         ))}
-
       </select>
-      <button type="submit" onClick={addselection} className="button-style">+ Add new schema</button>
-      
+    
+      <button type="submit" onClick={addselection} className="button-style">
+        + Add new schema
+      </button>
+     
     </div>
-   
   );
 }
 
